9bff01de80617ceda563481648d807a4168d4298
Bennett Feely <bfeelyweb@gmail.com>
2021-01-05 18:43:59 -0500

    new features

--- functions/root.js
+++ functions/root.js
@@ -1,3 +1,5 @@
+const randomColor = require("randomcolor");
+
 exports.handler = async function (event, context, callback) {
 	function rand(min, max) {
 		min = Math.ceil(min);
@@ -6,36 +8,46 @@ exports.handler = async function (event, context, callback) {
 	}
 
 	const date = new Date();
+	const colors = ["aliceblue", "antiquewhite", "aqua", "aquamarine", "azure", "beige", "bisque", "black", "blanchedalmond", "blue", "blueviolet", "brown", "burlywood", "cadetblue", "chartreuse", "chocolate", "coral", "cornflowerblue", "cornsilk", "crimson", "cyan", "darkblue", "darkcyan", "darkgoldenrod", "darkgray", "darkgreen", "darkgrey", "darkkhaki", "darkmagenta", "darkolivegreen", "darkorange", "darkorchid", "darkred", "darksalmon", "darkseagreen", "darkslateblue", "darkslategray", "darkslategrey", "darkturquoise", "darkviolet", "deeppink", "deepskyblue", "dimgray", "dodgerblue", "firebrick", "floralwhite", "forestgreen", "fuchsia", "gainsboro", "ghostwhite", "gold", "goldenrod", "gray", "green", "greenyellow", "grey", "honeydew", "hotpink", "indianred", "indigo", "ivory", "khaki", "lavender", "lavenderblush", "lawngreen", "lemonchiffon", "lightblue", "lightcoral", "lightcyan", "lightgoldenrodyellow", "lightgray", "lightgreen", "lightgrey", "lightpink", "lightsalmon", "lightseagreen", "lightskyblue", "lightslategray", "lightslategrey", "lightsteelblue", "lightyellow", "lime", "limegreen", "linen", "magenta", "maroon", "mediumaquamarine", "mediumblue", "mediumorchid", "mediumpurple", "mediumseagreen", "mediumslateblue", "mediumspringgreen", "mediumturquoise", "mediumvioletred", "midnightblue", "mintcream", "mistyrose", "moccasin", "navajowhite", "navy", "oldlace", "olive", "olivedrab", "orange", "orangered", "orchid", "palegoldenrod", "palegreen", "paleturquoise", "palevioletred", "papayawhip", "peachpuff", "peru", "pink", "plum", "powderblue", "purple", "rebeccapurple", "red", "rosybrown", "royalblue", "saddlebrown", "salmon", "sandybrown", "seagreen", "seashell", "sienna", "silver", "skyblue", "slateblue", "slategray", "slategrey", "snow", "springgreen", "steelblue", "tan", "teal", "thistle", "tomato", "turquoise", "violet", "wheat", "white", "whitesmoke", "yellow", "yellowgreen"];
 
 	const output = `:root {
 	--random: ${Math.random()};
-	--random-int-1: ${rand(0, 1)};
-	--random-int-10: ${rand(0, 10)};
-	--random-int-100: ${rand(0, 100)};
-	--random-int-1000: ${rand(0, 1000)};
-	--random-int-10000: ${rand(0, 10000)};
-	--random-int-100000: ${rand(0, 100000)};
-	--random-int-1000000: ${rand(0, 1000000)};
+
+	--random-light-hex: ${randomColor({ luminosity: "light" })};
+	--random-dark-hex: ${randomColor({ luminosity: "dark" })};
 
 	--random-rgb: rgb(${rand(0, 255)}, ${rand(0, 255)}, ${rand(0, 255)});
-	--random-rgba: rgba(${rand(0, 255)}, ${rand(0, 255)}, ${rand(0,255)}, ${Math.random()});
-	--random-hsl: hsla(${rand(0, 360)}, ${rand(0, 100)}%, ${rand(0,100)}%, ${Math.random()});
+	--random-rgba: rgba(${rand(0, 255)}, ${rand(0, 255)}, ${rand(0,255)}, ${Math.random().substring(0, 4)});
+	--random-hsl: hsla(${rand(0, 360)}, ${rand(0, 100)}%, ${rand(0,100)}%, ${Math.random().substring(0, 4)});
 	--random-hsla: hsl(${rand(0, 360)}, ${rand(0, 100)}%, ${rand(0, 100)}%);
-	--random-hex: #${Math.floor(Math.random() * 16777215).toString(16)};
-	--random-hex-3: #${Math.floor(Math.random() * 16777215).toString(16).substring(0, 3)};
+	--random-hex: #${randomColor({ luminosity: "random", hue: "random" })};
+	--random-hex-3: #${randomColor({ luminosity: "random", hue: "random" }).substring(0, 3)};
+	--random-color-keyword: ${colors[rand(0, colors.length)]};
 
+	--golden-ratio: 1.618033988749894;
 	--pi: ${Math.PI};
 	--e: ${Math.E};
-	--golden-ratio: 1.618033988749894;
 
-	--utc-date: ${date.getUTCDate()};
-	--utc-day: ${date.getUTCDay()};
-	--utc-full-year: ${date.getUTCFullYear()};
-	--utc-hours: ${date.getUTCHours()};
-	--utc-milliseconds: ${date.getUTCMilliseconds()};
-	--utc-minutes: ${date.getUTCMinutes()};
-	--utc-month: ${date.getUTCMonth()};
+	--random-boolean: ${rand(0, 1)};
+
+	--year: ${date.getUTCFullYear()};
 }`;
+
+// --utc-date: ${date.getUTCDate()};
+// --utc-day: ${date.getUTCDay()};
+// --utc-full-year: ${date.getUTCFullYear()};
+// --utc-hours: ${date.getUTCHours()};
+// --utc-milliseconds: ${date.getUTCMilliseconds()};
+// --utc-minutes: ${date.getUTCMinutes()};
+// --utc-month: ${date.getUTCMonth()};
+
+// 	--sqrt2: ${Math.SQRT2};		/* returns the square root of 2  */
+// 	--sqrt1_2: ${Math.SQRT1_2};	/* returns the square root of 1/2 */
+// 	--ln2: ${Math.LN2};			/* returns the natural logarithm of 2 */
+// 	--ln10: ${Math.LN10};		/* returns the natural logarithm of 10 */
+// 	--log2e: ${Math.LOG2E};		/* returns base 2 logarithm of E */
+// 	--log10e: ${Math.LOG10E};	/* returns base 10 logarithm of E */
+
 
 	return {
         headers: {
